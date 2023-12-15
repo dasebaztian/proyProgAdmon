5ae06daed99775a9bab700e1f25399ac4e8ea6ee
Sebastián Sánchez <danielsebastiansanchezm@gmail.com>
2023-12-14 19:10:21 -0600

    Cambios en backup y readme

--- backup.sh
+++ backup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-date >> "/tmp/backup.log"
+
+montarUSB() {
+	local dir="/montar"
+	echo "Empece la funcion" >> "/tmp/backup.log"
+	echo "Usuario $USER" >> "/tmp/backup.log"
+	mount /dev/sda /montar
+	echo "$?" >> "/tmp/backup.log"
+	openssl dgst -sign "$dir/privatekey.pem" -out "$dir/signature.bin" -sha256 "$dir/signature"
+	openssl dgst -verify "/home/dasebaztian/proyProgAdmon/publickey.pem" -signature "$dir/signature.bin" -sha256 signature && echo "Si sirven las firmas" >> "/tmp/backup.log"
+}
+
+montarUSB
+
